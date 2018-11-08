49aad1ece715fea85836d8029ad162267fa6ea63
vikvo11 <34969699+vikvo11@users.noreply.github.com>
2018-11-08 20:32:57 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,103 @@
+#import pymongo
+#from pymongo import MongoClient
+#import json
+#import pymongo
+#from bson import BSON
+#from bson import json_util
+from flask import Flask, flash, redirect, render_template, request, session, abort,url_for,logging #For work with HTTP and templates
+import requests # For HTTP requests
+#from functools import wraps # For lock access
+#from HTTP_basic_Auth import auths # For lock access
+
+#client = MongoClient("ds141786.mlab.com:41786", username = 'podarkin', password = 'podarkin', authSource = 'heroku_q51pzrtm')
+#db = client["heroku_q51pzrtm"]
+#bookings_coll = db.bookings
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'morkovka18'
+#Check if user logged in
+#def is_logged_in(f):
+#    @wraps(f)
+#    def wrap(*args,**kwargs):
+#        if 'logged_in' in session:
+#            return f(*args,**kwargs)
+#        else:
+            #flash('Unauthorized, Please login', 'danger')
+#            return redirect(url_for('login'))
+#    return wrap
+
+#@app.route('/')
+#@is_logged_in
+
+def home():
+    a=1
+    return redirect(url_for('dashbord'))
+
+'''
+@app.route('/test1')
+def test():
+    msg = py()
+    return render_template('home.html', articles=msg)
+'''
+#Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out','success')
+    return redirect(url_for('login'))
+
+#User Login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        #Get Form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+        #users=auths()
+        if auths(username,password_candidate):
+            session['logged_in']= True
+            return redirect(url_for('dashbord'))
+        else:
+                error='Invalid login'
+                return render_template('login.html',error=error)
+
+    return render_template('login.html')
+
+#Dashbord
+@app.route('/dashbord',methods=['GET','POST'])
+def dashbord():
+    msg = py()
+    return render_template('dashbordpymongo.html', articles='test')
+
+#Dashbord
+@app.route('/main',methods=['GET','POST'])
+def main():
+    #msg = py()
+    #return render_template('index.html')
+
+	header={'phone':'777-777-7777','email':'e-mail: info@ladymarlene.ru','link':'https://vk.com/ladymarlene','infolink':'vk.com/ladymarlene','city':'г. Москва'}
+	source={'logo':'source/logo.png','block1':'img/05.jpg'}
+	return render_template('dashbord_lady.html', header=header,source=source)	
+	
+def py():
+    #client = MongoClient("ds141786.mlab.com:41786", username = 'podarkin', password = 'podarkin', authSource = 'heroku_q51pzrtm')
+    #db = client["heroku_q51pzrtm"]
+    #bookings_coll = db.bookings
+    #doc = bookings_coll.find_one()
+    #asa = json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
+    #docs = bookings_coll.find()
+    #id = docs[0]['name']
+    #return docs
+	return None
+
+def main():
+    #doc = bookings_coll.find_one()
+
+
+    pass
+   # a = [x for x in bookings_coll.find()]
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+    #main()
